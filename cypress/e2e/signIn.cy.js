8ad97e5153c351bc9a098170927281aed859f47f
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-04-09 17:56:08 +0200

    e2e_intro

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,17 +1,24 @@
 /// <reference types="cypress" />
 
+/// <reference types="cypress" />
+
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.fixture('user').as('userData');
+  });
+
+  it('should provide an ability to log in', function () {
     cy.visit('https://react-redux.realworld.io');
 
     cy.contains('Sign in').click();
 
-    cy.get('input[type="email"]').type('my_email@example.com');
-    cy.get('input[type="password"]').type('asdfg3');
+    cy.get('input[type="email"]').type(this.userData.email);
+    cy.get('input[type="password"]').type(this.userData.password);
 
     cy.get('button[type="submit"]').click();
 
-    cy.get('nav').should('contain', 'YourUsername');
+    cy.get('nav').should('contain', this.userData.username);
   });
 });
+
 
